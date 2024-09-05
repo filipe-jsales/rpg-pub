5dab12970450dc18e969bb515d960bc41bacab8d
filipe-sales <eduardojs999@gmail.com>
2024-09-05 00:13:19 -0300

    wip: basic scripts player movement and dialog

---
+++ Assets/Scripts/DialogManager.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour
+{
+    [SerializeField] GameObject dialogBox;
+    [SerializeField] Text dialogText;
+    [SerializeField] int lettersPerSecond = 5;
+    public static DialogManager Instance { get; private set; }
+
+    public event Action OnShowDialog;
+    public event Action OnHideDialog;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    Dialog dialog;
+    int currentLine = 0;
+    bool isTyping;
+
+    public IEnumerator ShowDialog(Dialog dialog)
+    {
+        yield return new WaitForEndOfFrame();
+        OnShowDialog?.Invoke();
+        this.dialog = dialog;
+        dialogBox.SetActive(true);
+        StartCoroutine(TypeDialog(dialog.Lines[0]));
+    }
+    public void HandleUpdate()
+    {
+        if (Input.GetKeyUp(KeyCode.Z) && !isTyping)
+        {
+            ++currentLine;
+            if (currentLine < dialog.Lines.Count)
+            {
+                StartCoroutine(TypeDialog(dialog.Lines[currentLine]));
+            }
+            else
+            {
+                currentLine = 0;
+                dialogBox.SetActive(false);
+                OnHideDialog?.Invoke();
+            }
+        }
+    }
+
+    public IEnumerator TypeDialog(string Line)
+    {
+        isTyping = true;
+        dialogText.text = "";
+        foreach (var letter in Line.ToCharArray())
+        {
+            dialogText.text += letter;
+            yield return new WaitForSeconds(1f / lettersPerSecond);
+        }
+        isTyping = false;
+    }
+
+}

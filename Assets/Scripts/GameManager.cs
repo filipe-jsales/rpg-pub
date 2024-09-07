5d89dba3a3ca51c333f6ec85c5fba8d28de4edab
filipe-sales <eduardojs999@gmail.com>
2024-09-07 19:35:02 -0300

    fix: knockback bug not appearing

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,8 +5,8 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
-    //[SerializeField] float levelLoadDelay = 1f;
     [SerializeField] int playerLives = 3;
+
     private void Awake()
     {
         if (instance == null)
@@ -19,17 +19,31 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
     public void ProcessPlayerDamageTaken()
     {
         if (playerLives > 1)
         {
-            TakeLife();
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            StartCoroutine(HandlePlayerDamage());
         }
         else
         {
-            ResetGameSession();
+            StartCoroutine(HandlePlayerDeath());
         }
+    }
+
+    private IEnumerator HandlePlayerDamage()
+    {
+        yield return new WaitForSeconds(2);
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private IEnumerator HandlePlayerDeath()
+    {
+        yield return new WaitForSeconds(2);
+
+        ResetGameSession();
     }
 
     void ResetGameSession()
@@ -41,9 +55,7 @@ public class GameManager : MonoBehaviour
     public void TakeLife()
     {
         playerLives--;
-        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-        SceneManager.LoadScene(currentSceneIndex);
-        //TODO: change the logic to no longer reset the level, but instead add an animation of the player taking damage and being invincible for a few seconds
+        Debug.Log("Vida restante: " + playerLives);
     }
 
     public void QuitGame()

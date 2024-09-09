18f2675be32a9d82174adb675f30e5e1e388dafa
filipe-sales <eduardojs999@gmail.com>
2024-09-09 13:48:14 -0300

    feat: Add coin pickup functionality and sound effect
    - Added a boolean flag to track if the coin has been collected
    - Updated OnTriggerEnter2D method to check if the coin has not been collected before awarding points and playing sound effect
    - Deactivated the coin game object after it has been collected

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -6,12 +6,15 @@ public class Pickup : MonoBehaviour
 {
     [SerializeField]
     private int pointPerCoin = 1;
+    private bool wasCollected = false;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if (collision.tag == "Player" && !wasCollected)
         {
+            wasCollected = true;
             FindFirstObjectByType<GameManager>().AddToScore(pointPerCoin);
             AudioManager.instance.PlayAtPoint("Coin Pickup");
+            gameObject.SetActive(false);
             Destroy(gameObject);
         }
     }
